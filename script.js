8721fc372d07c7acb179e544ee2550916bed1fb6
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-11-24 08:41:37 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 setInterval(function(){
     
     let novaHora = new Date();
+    let data = new Date()
     // getHours trará a hora
     // geMinutes trará os minutos
     // getSeconds trará os segundos
@@ -8,9 +9,12 @@ setInterval(function(){
     let minuto = novaHora.getMinutes();
     let segundo = novaHora.getSeconds();
 
-    let dia = novaHora.getUTCDay();
-    let mes = novaHora.getMonth();
-    let ano = novaHora.getUTCFullYear();
+    let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Augosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+    let dia = data.getDate();
+    let mes = meses[data.getMonth()];
+    let ano = novaHora.getFullYear();
+    
     
 
     // Chamamos a função zero para que ela retorne a concatenação
@@ -18,14 +22,16 @@ setInterval(function(){
     
     minuto = zero(minuto);
     segundo = zero(segundo);
-    dia = zero(dia)
-    mes = zero(mes)
+    
     
 
     // Com o textContent, iremos inserir as horas, minutos e segundos
     // no nosso elemento HTML
-    document.getElementById('hora').textContent = hora+':'+minuto+':'+segundo+' | '+dia+'/'+mes+'/'+ano;
+    document.getElementById('hora').innerHTML = hora+':'+minuto+':'+segundo+' <br> '+dia+'/'+mes+'/'+ano;
 },1000)
+
+
+
 
 // A function zero concatena a string (número) 0 em frente aos números
 // mantendo o zero na frente dos números menores que 10. Exemplo:
