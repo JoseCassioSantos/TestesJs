1d72a5fb1c1ecd505eb6d38aeae257c5d31b65c6
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-11-26 19:08:06 -0300

    links create

---
+++ script/create.js
@@ -0,0 +1,20 @@
+function btnAdd(){
+    var nome = window.prompt('Nome do link')
+    var link = window.prompt('Insira o link')
+    var att = document.createAttribute('href')
+    var tar = document.createAttribute('target')
+    tar.value = '_blank'
+    att.value = link
+    var btn = document.createElement('a')
+    btn.innerHTML = nome 
+    btn.setAttributeNode(att)
+    btn.setAttributeNode(tar)
+    document.body.appendChild(btn)
+
+}
+
+//localStorage.setItem("Nome","cassio")
+
+//var vnome = localStorage.getItem("Nome")
+
+//document.write(vnome)
