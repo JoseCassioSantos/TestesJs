9a40b18653df407a8a492fe44013448dcc408e22
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-11-26 00:11:22 -0300

    links update

--- script/script.js
+++ script/script.js
@@ -50,6 +50,18 @@ function zero(x) {
     } return x;
 }
 
-function fecha(){
+function NeoserviceM(){
     myWindow = window.open("https://neoservice.neobpo.com.br/services/navigation", "_blank", "width=500, height=500");
+}
+
+function Neoservice(){
+    myWindow = window.open("https://neoservice.neobpo.com.br/tas/public/login/saml", "_blank", "width=500, height=500");
+}
+
+function intranet(){
+    myWindow = window.open("https://intranet.neobpo.com.br/", "_blank", "width=500, height=500");
+}
+
+function cortina(){
+    myWindow = window.open("https://punchclock2.neobpo.com.br/PunchClockClient/Forms/Login.aspx?ReturnUrl=%2fPunchClockClient%2f", "_blank", "width=500, height=500");
 }
