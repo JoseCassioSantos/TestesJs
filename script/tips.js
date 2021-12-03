cec4efd0303c01b971a9aee1e322cf98d2cf9175
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-12-03 07:47:01 -0300

    tips update

--- script/tips.js
+++ script/tips.js
@@ -1,6 +1,21 @@
-function tip() {
+function tip1() {
   
-    var copyText = document.getElementById("tip1");
+    let copyText = document.getElementById("tip1");
+  
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    window.alert("Copiado")
+    
+  }
+
+  function tip2() {
+  
+    let copyText = document.getElementById("tip2");
   
     
     copyText.select();
