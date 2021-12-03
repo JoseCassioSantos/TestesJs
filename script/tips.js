d9c6f56a6a36ed263bd39954ed2f131c4b676cf6
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-12-03 13:35:48 -0300

    cortina tips

--- script/tips.js
+++ script/tips.js
@@ -27,3 +27,18 @@ function tip1() {
     window.alert("Copiado")
     
   }
+
+  function tip3() {
+  
+    let copyText = document.getElementById("tip3");
+  
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    window.alert("Copiado")
+    
+  }
